e6b04fcc794096e5853dfbcb95bbbc219b30e3e7
Jesie Oldenburg <kitglo46@gmail.com>
2020-04-18 01:12:26 -0500

    initial model setup and migrations complete

---
+++ F1App/models/drivers.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import F
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Drivers(models.Model):
+
+    name = models.CharField(max_length=30, null=True, blank=True)
+    team = models.CharField(max_length=50, null=True, blank=True)
+    teammate = models.CharField(max_length=30, null=True, blank=True)
+    season_points = models.IntegerField()
+    championships = models.IntegerField()
+    wins = models.IntegerField()
